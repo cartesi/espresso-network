684bdfdf8ecce105c715598e06cb8ec5a179bbc7
imabdulbasit <imabdulbasit@users.noreply.github.com>
2025-03-11 15:48:18 +0000

    deploy: ce2a014b318ee42ef27153f6298ae2738971b30b

--- hotshot_types/vid/avidm/sidebar-items.js
+++ hotshot_types/vid/avidm/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["init_avidm_param"],"type":["AvidMCommitment","AvidMParam","AvidMScheme","AvidMShare"]};
+window.SIDEBAR_ITEMS = {"fn":["init_avidm_param"],"type":["AvidMCommitment","AvidMCommon","AvidMParam","AvidMScheme","AvidMShare"]};
