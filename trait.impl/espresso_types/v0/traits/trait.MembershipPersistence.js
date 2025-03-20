93b59b61d2a152a037ff8d7a5a476b8c05da21ed
sveitser <sveitser@users.noreply.github.com>
2025-03-20 14:59:50 +0000

    deploy: f04e579f8213567772c9c5b460a55f85ff9e336d

--- trait.impl/espresso_types/v0/traits/trait.MembershipPersistence.js
+++ trait.impl/espresso_types/v0/traits/trait.MembershipPersistence.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["sequencer",[["impl <a class=\"trait\" href=\"espresso_types/v0/traits/trait.MembershipPersistence.html\" title=\"trait espresso_types::v0::traits::MembershipPersistence\">MembershipPersistence</a> for <a class=\"struct\" href=\"sequencer/persistence/fs/struct.Persistence.html\" title=\"struct sequencer::persistence::fs::Persistence\">Persistence</a>"],["impl <a class=\"trait\" href=\"espresso_types/v0/traits/trait.MembershipPersistence.html\" title=\"trait espresso_types::v0::traits::MembershipPersistence\">MembershipPersistence</a> for <a class=\"struct\" href=\"sequencer/persistence/sql/struct.Persistence.html\" title=\"struct sequencer::persistence::sql::Persistence\">Persistence</a>"]]]]);
+    var implementors = Object.fromEntries([["sequencer",[["impl MembershipPersistence for <a class=\"struct\" href=\"sequencer/persistence/fs/struct.Persistence.html\" title=\"struct sequencer::persistence::fs::Persistence\">Persistence</a>"],["impl MembershipPersistence for <a class=\"struct\" href=\"sequencer/persistence/sql/struct.Persistence.html\" title=\"struct sequencer::persistence::sql::Persistence\">Persistence</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[701]}
+//{"start":57,"fragment_lengths":[389]}
