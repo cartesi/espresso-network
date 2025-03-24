3fe5c048ab95404a47c69e0b5c77e559920fb186
Rob <rob@espressosys.com>
2025-03-24 16:02:56 -0400

    default public hs config

--- types/src/v0/config.rs
+++ types/src/v0/config.rs
@@ -74,7 +74,12 @@ pub struct PublicHotShotConfig {
     start_voting_time: u64,
     stop_voting_time: u64,
     epoch_height: u64,
+    #[serde(default = "default_epoch_start_block")]
     epoch_start_block: u64,
+}
+
+fn default_epoch_start_block() -> u64 {
+    1
 }
 
 impl From<HotShotConfig<PubKey>> for PublicHotShotConfig {
