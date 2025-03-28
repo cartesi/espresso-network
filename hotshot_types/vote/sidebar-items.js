7849977b7fdbc611304744be78521f3faf42a135
mrain <mrain@users.noreply.github.com>
2025-03-28 17:11:00 +0000

    deploy: 5d0baf07cbb27f4ac63df116bc3ff78545554cdb

--- hotshot_types/vote/sidebar-items.js
+++ hotshot_types/vote/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["VoteAccumulator"],"trait":["Certificate","HasViewNumber","Vote"],"type":["SignersMap","VoteMap2"]};
+window.SIDEBAR_ITEMS = {"struct":["LightClientStateUpdateVoteAccumulator","VoteAccumulator"],"trait":["Certificate","HasViewNumber","Vote"],"type":["SignersMap","VoteMap2"]};
--- sequencer/state_signature/sidebar-items.js
+++ sequencer/state_signature/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["SIGNATURE_STORAGE_CAPACITY"],"fn":["form_light_client_state","hash_bytes_to_field","static_stake_table_commitment"],"mod":["relay_server"],"struct":["StateSignatureMemStorage","StateSigner"],"type":["StakeTableCommitmentType"]};
+window.SIDEBAR_ITEMS = {"constant":["SIGNATURE_STORAGE_CAPACITY"],"fn":["static_stake_table_commitment"],"mod":["relay_server"],"struct":["StateSignatureMemStorage","StateSigner"],"type":["StakeTableCommitmentType"]};
