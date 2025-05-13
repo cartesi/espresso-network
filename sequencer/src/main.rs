85b73140a54794f5c905a1c5161081569e980719
Rob <rob@espressosys.com>
2025-05-13 13:11:09 -0400

    make it an env var

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,7 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, env = "TOKIO_THREAD_STACK_SIZE")]
+    tokio_thread_stack_size: Option<usize>,
+}
+
 pub fn main() -> anyhow::Result<()> {
+    // Parse args
+    let args = Args::parse();
+
+    // Set tokio thread stack size
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
-        .thread_stack_size(4194304)
+        .thread_stack_size(args.tokio_thread_stack_size.unwrap_or(4194304))
         .build()
         .unwrap();
 
