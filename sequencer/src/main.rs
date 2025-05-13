291f384350d57823cae2a4cb6757ac048f99c24c
Rob <rob@espressosys.com>
2025-05-13 10:39:22 -0400

    increase thread stack size

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,4 +1,9 @@
-#[tokio::main]
-pub async fn main() -> anyhow::Result<()> {
-    sequencer::main().await
+pub fn main() -> anyhow::Result<()> {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_stack_size(4194304)
+        .build()
+        .unwrap();
+
+    rt.block_on(sequencer::main())
 }
