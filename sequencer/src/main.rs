a53d546b96c805412b6f9b4394c6de622cc92c10
Rob <rob@espressosys.com>
2025-05-16 09:35:16 -0400

    stack size

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,4 +1,16 @@
-#[tokio::main]
-pub async fn main() -> anyhow::Result<()> {
-    sequencer::main().await
+pub fn main() -> anyhow::Result<()> {
+    // Get the environment variable
+    let tokio_thread_stack_size = std::env::var("TOKIO_THREAD_STACK_SIZE")
+        .unwrap_or("4194304".to_string())
+        .parse::<usize>()
+        .unwrap_or(4194304);
+
+    // Set tokio thread stack size
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_stack_size(tokio_thread_stack_size)
+        .build()
+        .unwrap();
+
+    rt.block_on(sequencer::main())
 }
